28045ae5937356faff2e375cb90220f47da0b08c
hadymaher311 <hadymaher311@gmail.com>
2022-11-15 14:58:01 +0200

    fix package setup command

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,7 @@
 docker run -d --name keycloak_extend_test -p 8180:8180 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:latest start-dev --http-port 8180
 pip install pip --upgrade
 pip install -r  requirements.txt
-python3 setup.py .
+pip install -e .
 ./wait-for-it.sh localhost:8180 -t 60
 coverage run -m pytest -s tests
 coverage report -m
