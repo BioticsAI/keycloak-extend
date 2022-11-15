7a45472109e7003f5201e60a8061bfe845e29118
hadymaher311 <hadymaher311@gmail.com>
2022-11-15 15:27:37 +0200

    fix package setup command

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from setuptools import setup
 
-setup(name='keycloak_extend')
+setup(
+    name="keycloak_extend",
+    version="0.0.1",
+    description="Python keycloak extension for python-keycloak package",
+    author="BioticsAI",
+)
