971534571ebe04e91d8fd48b8eb6a692b68aad15
hadymaher311 <hadymaher311@gmail.com>
2022-11-15 14:54:39 +0200

    add github actions to test PRs

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+docker run -d --name keycloak_extend_test -p 8180:8180 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:latest start-dev --http-port 8180
+pip install pip --upgrade
+pip install -r  requirements.txt
+python3 setup.py .
+./wait-for-it.sh localhost:8180 -t 60
+coverage run -m pytest -s tests
+coverage report -m
+coverage xml -o code-coverage.xml
