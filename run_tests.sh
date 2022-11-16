969e43624710a8762d72d1160695278ffe82ad8c
hadymaher311 <hadymaher311@gmail.com>
2022-11-16 11:02:04 +0200

    change port

--- run_tests.sh
+++ run_tests.sh
@@ -4,7 +4,7 @@ pip install pip --upgrade
 pip install -r  requirements.txt
 pip install .
 pip install pyOpenSSL --upgrade
-./wait-for-it.sh keycloak:8180 -t 60
+./wait-for-it.sh keycloak:8080 -t 60
 coverage run -m pytest -s tests
 coverage report -m
 coverage xml -o code-coverage.xml
