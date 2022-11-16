a8baafbbfec18b82ac41b10cfc5ce51194e5da42
hadymaher311 <hadymaher311@gmail.com>
2022-11-16 10:07:39 +0200

    remove venv

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,6 @@
 
 docker run -d --name keycloak_extend_test -p 8180:8180 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:latest start-dev --http-port 8180
 pip install pip --upgrade
-pip install virtualenv
-python -m venv venv
-chmod +x ./venv/bin/activate
-./venv/bin/activate
 pip install -r  requirements.txt
 pip install .
 pip install pyOpenSSL --upgrade
