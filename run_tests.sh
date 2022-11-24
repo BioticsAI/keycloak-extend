d621783466c4b68c513411d419e7c70bd26efc8b
hadymaher311 <hadymaher311@gmail.com>
2022-11-24 12:30:21 +0200

    run python test

--- run_tests.sh
+++ run_tests.sh
@@ -2,9 +2,12 @@
 
 # docker ps
 docker run -d --name keycloak -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:latest start-dev
+sleep 10
+docker ps
+docker logs keycloak
 pip install pip --upgrade
 pip install -r  requirements.txt
 pip install .
 pip install pyOpenSSL --upgrade
 ./wait-for-it.sh localhost:8080 -t 60
-pytest -s tests
+python -m pytest -s tests
