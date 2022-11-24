a53ffe2a9bc5c8532d6e5e81f6b632a06427112d
hadymaher311 <hadymaher311@gmail.com>
2022-11-24 10:51:04 +0200

    run keycloak container from sh file

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 docker ps
-sleep 10
+docker run --name keycloak -p 8080:8080 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:latest start-dev
+sleep 30
+docker ps
 docker logs keycloak
 pip install pip --upgrade
 pip install -r  requirements.txt
